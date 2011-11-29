fd0f47e676d6f65dd6c35d40b199612c29dd1d26
John Lohavichan <john.lohavichan@gmail.com>
2011-11-29 13:52:57 -0500

    Removed public rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
-SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+SampleApp::Application.routes.draw do  
+  root :to => 'pages#home'
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  
+  # Old version used with Pages Controller
+  #get "users/new"
+  #get "pages/home"
+  #get "pages/contact"
+  #get "pages/about"
+  #get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
