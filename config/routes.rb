106d510de85d82ef58ddf027f7bd80313fd2f738
John Lohavichan <john.lohavichan@gmail.com>
2011-11-30 10:27:41 -0500

    Finished Layouts and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do  
+  get "users/new"
+  match '/signup',  :to => 'users#new'
+  
   root :to => 'pages#home'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  
   
   
   # Old version used with Pages Controller
