f7745eff6c53e51b4b453653271a0518c5436549
John Lohavichan <john.lohavichan@gmail.com>
2011-12-07 20:24:52 -0500

    Updated config with assets precompile fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,10 @@ SampleApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  
+  # Add Precompile for blueprint because throwing a precompile error in Heroku...
+  # http://stackoverflow.com/questions/7443536/blueprint-screen-css-isnt-precompiled
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
